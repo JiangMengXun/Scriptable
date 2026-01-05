5734a67933fb0eb5b766f36c1ebeef7fd6a01566
JiangMengXun <95838995+JiangMengXun@users.noreply.github.com>
2026-01-06 05:40:17 +0800

    Add files via upload

--- LRT_Timer29D18.js
+++ LRT_Timer29D18.js
@@ -277,6 +277,7 @@ if (config.runsInWidget || SILENT_RUN) {
   //const footer1 = w.addText(`\n🏁 程式總耗時P：${SUM_COST} 秒  \n ${SCRIPT_NAME} ${VERSION}`);
   //footer1.font = Font.systemFont(14);
   //footer1.textColor = Color.black();
+  COST_GPS_STATION=COST_GPS_STATION*2;
   const debug = w.addText(
     `GPS：${COST_GPS_STATION}s  API：${COST_API}s\n` +
     `PARSE：${COST_PARSE}s  UI：${COST_UI}s\n` +
