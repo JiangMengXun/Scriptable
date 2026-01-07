36626c8a6400ccaf0571fa0e7bf3637cf97850de
JiangMengXun <95838995+JiangMengXun@users.noreply.github.com>
2026-01-08 07:29:52 +0800

    Add files via upload

--- LRT_Timer29D19.js
+++ LRT_Timer29D19.js
@@ -289,11 +289,11 @@ if (config.runsInWidget || SILENT_RUN) {
   const COST_GPS_STATION20=Number(obj.COST_GPS_STATION2)|| 0;
   const COST_tAPI20=Number(obj.COST_tAPI2)|| 0;
   const COST_tPARSE20=Number(obj.COST_tPARSE2)|| 0;  
-  const result20=COST_GPS_STATION20+COST_tAPI20+COST_tPARSE20;
+  const result20=COST_GPS_STATION20+COST_tAPI20+COST_tPARSE20+5.0;
   const debug = w.addText(
     `GPS0：${result.toFixed(2)} s  API：${COST_API}s\n` +
     `PARSE：${COST_PARSE}s  UI：${COST_UI}s\n` +
-    `TOTAL：${TOTAL_COST}s  GPS0：${COST_GPS_STATION20.toFixed(2)} s`+
+    `TOTAL：${TOTAL_COST}s  GPS0：${COST_GPS_STATION20.toFixed(2)} s`\n+
     `TOTAL1：${result20.toFixed(2)} s`
   );
   debug.font = Font.systemFont(11);
